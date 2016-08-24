0f5c9461defcf27c347cb48b4a56125c2ceab96c
Frédérik Charest <Allov@users.noreply.github.com>
2016-08-24 08:49:46 -0400

    Clean up code

--- src/helpers/Helpers.js
+++ src/helpers/Helpers.js
@@ -1,7 +1,6 @@
-function font(settings) {
+export function font(settings) {
     return Object.assign({}, { font: '45px Courier New', fill: '#fff', align: 'center' }, settings);
 }
-exports.font = font;
 
 export function text(game, text, x, y, settings) {
     const t = game.add.text(x, y, text, font(settings));
@@ -9,6 +8,7 @@ export function text(game, text, x, y, settings) {
     return t;
 }
 
+// Extensions methods
 Math.rnd = (i, j) => {
     return Math.floor(Math.random() * j) + i;
 }
